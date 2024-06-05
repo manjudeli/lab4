672fb5e5ddd55c2a03c7db3f6001fc12c6348841
manjudeli <151948222+manjudeli@users.noreply.github.com>
2024-06-06 00:44:18 +0900

    Update proc.h

--- xv6/proc.h
+++ xv6/proc.h
@@ -49,7 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int setup_stack              // 추가
+  int setup_stack;             // 추가
 };
 
 // Process memory is laid out contiguously, low addresses first:
