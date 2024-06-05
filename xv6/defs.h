7eff22c2e6bbb8e615a67c694b5673cec3a3dda5
manjudeli <151948222+manjudeli@users.noreply.github.com>
2024-06-05 20:36:57 +0900

    Update defs.h

--- xv6/defs.h
+++ xv6/defs.h
@@ -120,6 +120,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int             printpt(int pid);    // 추가
 
 // swtch.S
 void            swtch(struct context**, struct context*);
